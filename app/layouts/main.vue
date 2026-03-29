21a263540e8f884b41a53020eb1a37f94e2ca30b
Ismail Basyir Muthahhar <Ismailbm31@gmail.com>
2026-03-29 08:17:12 +0700

    feat: up lainnya

---
+++ app/layouts/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="min-h-screen bg-[var(--fifth-color)]">
+    <MainNavbar />
+    <main class="bg-[var(--fifth-color)]">
+      <slot />
+    </main>
+    <MainFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MainFooter from '~/components/layout/MainFooter.vue';
+import MainNavbar from '~/components/layout/MainNavbar.vue';
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  app: {
+    baseURL: "/",
+    head: {
+      title: "Himakom Pokedex",
+      link: [{ rel: "icon", type: "image/png", href: "/assets/img/pokemon.png" }],
+    },
+  },
+
+  nitro: {
+    preset: "static",
+  },
+
+  modules: [
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss"
+  ],
+
+  css: ["./app/assets/css/main.css"],
+});
